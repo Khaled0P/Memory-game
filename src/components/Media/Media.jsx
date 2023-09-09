d27060b4d4178c20fd05b8e7445139501355f2fe
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-09-09 16:52:54 +0300

    reface background media to its own component
    move background media and change music from autoplay to play on click

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,13 +1,46 @@
 import video from '../../assets/videos/Adventure-time-Live-wallpaper.mp4';
 import audio from '../../assets/audio/background-audio.mp3';
-import { Fragment } from 'react';
-import './media.css';
+import { Fragment, useRef, useState } from 'react';
+import musicIcon from '../../assets/images/itunes.png';
+import styles from './Media.module.css';
 
 export default function Media() {
+  const backgroundMusic = useRef(new Audio(audio));
+  backgroundMusic.current.loop = true;
+  const [isPlaying, setIsPlaying] = useState(false);
+  function handleMusic() {
+    isPlaying
+      ? backgroundMusic.current.pause()
+      : backgroundMusic.current.play();
+
+    setIsPlaying(!isPlaying);
+  }
+
   return (
     <Fragment>
-      <video src={video} muted loop autoPlay />
-      <audio src={audio} autoPlay loop />
+      <video
+        className={styles.backgroundVideo}
+        src={video}
+        muted
+        loop
+        autoPlay
+      />
+      <button
+        onClick={handleMusic}
+        className={
+          isPlaying ? styles.backgroundIcon : styles.backgroundIconMuted
+        }
+      >
+        {' '}
+        <img src={musicIcon} alt="music icon" />
+      </button>
+      <a
+        className={styles.credit}
+        href="https://www.flaticon.com/free-icons/itunes"
+        title="itunes icons"
+      >
+        Music icon created by Freepik - Flaticon
+      </a>
     </Fragment>
   );
 }
